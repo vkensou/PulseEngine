fbf1857a5f17e04792251917794ee42fe31c23c6
vkensou <boyplaydota@hotmail.com>
2026-03-31 14:36:20 +0800

    使用包引用第三方库

--- dascript/xmake.lua
+++ dascript/xmake.lua
@@ -7,20 +7,12 @@ elseif is_plat("unix") then
     add_cxflags("-Wno-strict-aliasing")
 end
 
--- add_requires("Threads")
-
-
-target("libUriParser")
-    set_kind("static")
-    add_files("3rdparty/uriparser/src/*.c")
-    add_includedirs("3rdparty/uriparser/include")
-    add_defines("URIPARSER_BUILD_CHAR", "URI_STATIC_BUILD")
+add_requires("fmt <12.0.0")
+add_requires("uriparser")
 
 target("libDaScript")
     set_kind("static")
     add_includedirs("include", {public = true})
-    add_includedirs("3rdparty/fmt/include")
-    add_includedirs("3rdparty/uriparser/include")
     add_includedirs("include/daScript")
     add_includedirs("include/vecmath")
     add_includedirs("include/ska")
@@ -32,8 +24,8 @@ target("libDaScript")
     add_files("src/hal/*.cpp")
     add_files("src/misc/*.cpp")
     add_files("src/builtin/*.cpp")
-    add_files("3rdparty/fmt/src/format.cc")
-    add_deps("libUriParser")
+    add_packages("fmt")
+    add_packages("uriparser")
     if is_plat("windows") then
         add_links("dbghelp")
     elseif is_plat("linux") or is_plat("macosx") then
