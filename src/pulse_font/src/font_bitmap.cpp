236657af66736019574fa56a8f7e4c1e2a9e4631
vkensou <boyplaydota@hotmail.com>
2026-09-17 15:30:04 +0800

    禁用 pulse_font 里 stb_image.h 的若干功能，减少二进制大小。

--- src/pulse_font/src/font_bitmap.cpp
+++ src/pulse_font/src/font_bitmap.cpp
@@ -2,6 +2,18 @@
 
 #include "pulse_vfs.h"
 
+#define STBI_NO_JPEG
+#define STBI_NO_BMP
+#define STBI_NO_PSD
+#define STBI_NO_GIF
+#define STBI_NO_PIC
+#define STBI_NO_PNM
+#define STBI_NO_TGA
+#define STBI_NO_HDR
+#define STBI_NO_LINEAR
+#define STBI_NO_STDIO
+#define STBI_NO_FAILURE_STRINGS
+#define STBI_NO_THREAD_LOCALS
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
